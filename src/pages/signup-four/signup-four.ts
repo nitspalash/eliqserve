168154bc01541cc41a12e5aa4c53805d74ee68f0
DebashreeNatitsolved <debashree.chandra@natitsolved.com>
2018-08-09 20:18:06 +0530

    debashree registration

--- src/pages/signup-four/signup-four.ts
+++ src/pages/signup-four/signup-four.ts
@@ -20,6 +20,19 @@ export class SignupFourPage {
   name:any;
   gender:any;
   dateOfBirth:any;
+  utype:any
+  log:any;
+  bank_details:any;
+trade_license:any;
+open_time:any;
+close_time:any;
+delivery:any;
+pickup:any;
+paypal_email:any;
+store_location:any;
+business_name:any;
+
+
   passwordmatch =true;
 itemEmail:any;
 email:any;
@@ -47,9 +60,23 @@ this.formGroup=new FormGroup
     console.log( this.item)
  this.name=this.item.name;
  this.gender=this.item.gender;
- this.dateOfBirth=this.item.dateOfBirth;
+ this.dateOfBirth=this.item.dob;
+ this.utype=this.item.utype;
+ this.log=this.item.log;
+ this.bank_details=this.item.bank_details;;
+ this.trade_license=this.item.trade_license;;
+ this.open_time=this.item.open_time;;
+ this.close_time=this.item.close_time;;
+ this.delivery=this.item.delivery;;
+ this.pickup=this.item.pickup;;
+ this.paypal_email=this.item.paypal_email;;
+ this.store_location=this.item.store_location;;
+ this.business_name=this.item.business_name;;
+
+
 this.itemEmail=JSON.parse(localStorage.getItem('email_address'));
 this.email=this.itemEmail.email
+
 
 console.log(this.email)
 console.log(this.name)
@@ -60,6 +87,20 @@ console.log(this.dateOfBirth)
     data.first_name=this.name;
     data.gender= this.gender;
     data.dob=this.dateOfBirth;
+
+    data.utype=this.utype;
+    data.log=this.log;
+
+    data.bank_details=this.bank_details
+    data.trade_license=this.trade_license
+    data.open_time=this.open_time
+    data.close_time=this.close_time
+    data.delivery=this.delivery
+    data.pickup=this.pickup
+    data.paypal_email=this.paypal_email
+    data.store_location=this.store_location
+    data.business_name=this.business_name
+
     data.phone=localStorage.getItem('ph_number');
     data.email=this.email
     console.log(data)
@@ -72,9 +113,9 @@ console.log(this.dateOfBirth)
       let detailResponse = res.details
       if(detailResponse.ack == 1){
         console.log('hello')
-       alert (detailResponse.message)
+      //  alert (detailResponse.message)
     
-        this.navCtrl.push('SignupFivePage')
+        this.navCtrl.setRoot('SignupFivePage')
         alert ('Please check your mail for otp');
       }
       else if(detailResponse.ack == 0){
--- src/pages/signupone/signupone.ts
+++ src/pages/signupone/signupone.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
 import {FormGroup,FormBuilder,FormControl, AbstractControl,Validators} from '@angular/forms'
 import {Storage} from '@ionic/storage'
 
@@ -26,15 +26,25 @@ formGroup:FormGroup;
 merchantForm:FormGroup
   constructor(public navCtrl: NavController, public navParams: NavParams,
   public formBuilder:FormBuilder,
+  public alertCtrl:AlertController,
 public storage: Storage) {
 
     this.formGroup=new FormGroup ({
       name: new FormControl ('',Validators.required),
       gender: new FormControl ('',Validators.required),
-      dateOfBirth: new FormControl ('',Validators.required),
+      dob: new FormControl ('',Validators.required),
       utype:new FormControl ('',Validators.required),
-    com_name:new FormControl ('',Validators.required),
-    log:new FormControl ('',Validators.required),
+
+        log:new FormControl (''),
+    bank_details:new FormControl (''),
+trade_license:new FormControl (''),
+open_time:new FormControl (''),
+close_time:new FormControl (''),
+delivery:new FormControl (''),
+pickup:new FormControl (''),
+paypal_email:new FormControl (''),
+store_location:new FormControl (''),
+business_name:new FormControl (''),
     })
 
   
@@ -51,8 +61,16 @@ public storage: Storage) {
 hideForm()
 {
   this.radio=false;
-this.formGroup.value.com_name='';
+this.formGroup.value.business_name='';
 this.formGroup.value.log='';
+this.formGroup.value.bank_details='';
+this.formGroup.value.trade_license='';
+this.formGroup.value.open_time='';
+this.formGroup.value.close_time='';
+this.formGroup.value.pickup='';
+this.formGroup.value.delivery='';
+this.formGroup.value.store_location='';
+this.formGroup.value.paypal_email='';
 }
 
 
@@ -60,14 +78,103 @@ this.formGroup.value.log='';
   {
 
     console.log(data);
-    console.log (this.formGroup.value.gender)
+    // console.log (this.formGroup.value.gender)
 
-    
+    console.log (this.formGroup.value.utype)
+
+    if (this.formGroup.value.utype==2)
+    {
+     if(!this.formGroup.value.business_name)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter your company name',
+               buttons: ['ok']
+      });
+      alert.present();
+     
+     } 
+     else if(!this.formGroup.value.log)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter your log name',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+     else if(!this.formGroup.value.store_location)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter your storelocation',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+     else if(!this.formGroup.value.open_time)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter your open time',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+     else if(!this.formGroup.value.close_time)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter your close time',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+
+     else if(!this.formGroup.value.delivery && !this.formGroup.value.pickup)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please select delivery and/or pickup',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+
+     else if(!this.formGroup.value.trade_license)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter trade license number',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+
+     else if(!this.formGroup.value.bank_details)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter bank details',
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+
+     else if(!this.formGroup.value.paypal_email)
+     {
+      let alert = this.alertCtrl.create({
+        title: 'Please enter paypal email', 
+               buttons: ['ok']
+      });
+      alert.present(); 
+     }
+
+     else
+     {
+      this.navCtrl.push('SignupTwoPage');
+     }
+    } else
+    {
+      this.navCtrl.push('SignupTwoPage');
+    }
 
     this.storage.ready().then(() => {
       this.userDataOne=localStorage.setItem ('userDataOne',JSON.stringify(data))
     });
-    this.navCtrl.push('SignupTwoPage');
+   
    
   }
   ionViewDidLoad() {
